bb5ee739c2af11f1efb97b545467d3ff11a8942a
takutaka <takutaka@pepabo.com>
2024-04-01 15:37:52 +0900

    add rbac

--- internal/controller/namespacedpv_controller.go
+++ internal/controller/namespacedpv_controller.go
@@ -45,6 +45,7 @@ type NamespacedPvReconciler struct {
 //+kubebuilder:rbac:groups=namespaced-pv.homi.run,resources=namespacedpvs/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=persistentvolumes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=persistentvolumes/finalizers,verbs=update;patch
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
